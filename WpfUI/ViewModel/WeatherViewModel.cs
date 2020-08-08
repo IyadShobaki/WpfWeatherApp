a594c5a836e3dce9099c015596f2939f22c5bfe1
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-08 14:49:20 -0400

    Adding the View and View Models
    Implement INotifyPropertyChanged interface.
    Binding context and design time binding.

---
+++ WpfUI/ViewModel/WeatherViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfUI.Model;
+
+namespace WpfUI.ViewModel
+{
+    public class WeatherViewModel : INotifyPropertyChanged
+    {
+        private string _query;
+
+        public string Query
+        {
+            get { return _query; }
+            set
+            {
+                _query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+        private CurrentConditions _currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return _currentConditions; }
+            set
+            {
+                _currentConditions = value;
+                OnPropertyChanged("CurrentConditions");
+            }
+        }
+
+        private City _selectedCity;
+
+        public City SelectedCity
+        {
+            get { return _selectedCity; }
+            set
+            {
+                _selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+
+        // For testing during the designing mode
+        public WeatherViewModel()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                SelectedCity = new City
+                {
+                    LocalizedName = "New York"
+                };
+                CurrentConditions = new CurrentConditions
+                {
+                    WeatherText = "Patly cloudy",
+                    Temperature = new Temperature
+                    {
+                        Metric = new Units
+                        {
+                            Value = 21
+                        }
+                    }
+                };
+
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
